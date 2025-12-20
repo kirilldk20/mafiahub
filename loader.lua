fd7665834d3b5d81cdbf81ea38aa4c56d8b26763
Handsome Jack <kirilldk11beloysov@gmail.com>
2025-12-20 23:30:22 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/4dad9ad9e6afd7f527312ef9cca88e00d79d8f3efebc6d20ff8a957d7d1e3dc8/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ed4e21790b24f2c65bc10853b676eee9543d1102b69eda51fb2625ab756f5492/download"))()
